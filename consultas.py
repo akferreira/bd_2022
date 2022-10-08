b35eaeecb156557bc8193046f6f26e60378e17ba
akferreira <49925514+akferreira@users.noreply.github.com>
2022-10-08 15:21:20 -0300

    Atualiza listas para enum

--- consultas.py
+++ consultas.py
@@ -1,121 +1,150 @@
 import re
+from enum import Enum
 
-listaSelect = [
-"SELECT IdLoja, IdCarro, AnoFab, ValorDiaria, TipoCombustivel, Placa, Opcionais",
-"SELECT Carros1.IdCarro, Placa, COUNT(CARROS1.IdCarro) as QtdManutencoes",
-'SELECT NomeCat, COUNT(NomeCat) as "Qtd Manutencoes"',
-"SELECT idFuncionario, Nome, IdLocacao, Carros1.IdCarro,NomeCat,Chassi,AnoFab,ValorDiaria,TipoCombustivel,Placa",
-"select round(avg(ValorDiaria),2) as Valor_diaria_medio",
-"select sum(Duracao)/count(*) as Duracao_Media",
-"SELECT IdLocatario, Nome, Email, Telefone, CARROS1.IdCarro, NomeCat, Placa, ValorDiaria, DataInicio, DataDevPrevista",
-"SELECT DISTINCT Nome, Email, Telefone",
-"select round(avg(custo),2) as custo_medio, max(custo) as conserto_mais_caro",
-"select count(mes || '-' || ano) as qtd,mes,ano from (select date_part('month',data) as mes, date_part('year',data) as ano",
-"select round(avg(custoLocacao),2) as Valor_medio_diaria",
-"select placa, idLoja,qtd",
-"select count(*) filter (where tipopag = false) as qtd_cartao, count(*) filter (where tipopag = true) as qtd_transferencia",
-"select greatest(qtd_cartao,qtd_transferencia) as qtd,(qtd_cartao < qtd_transferencia) as tipopag from (select count(*) filter (where tipopag = false) as qtd_cartao, count(*) filter (where tipopag = true) as qtd_transferencia",
-"SELECT IdCarro, PLACA, NomeCat,ValorDiaria"
-    ]
+
+
+class Consulta():
+    def __init__(self,select = "",descricao = "", comando = ""):
+        self.select = select
+        self.descricao = descricao
+        self.comando = comando
+
+
+class Menu(Enum):
+    OP1 = Consulta()
+    OP2 = Consulta()
+    OP3 = Consulta()
+    OP4 = Consulta()
+    OP5 = Consulta()
+    OP6 = Consulta()
+    OP7 = Consulta()
+    OP8 = Consulta()
+    OP9 = Consulta()
+    OP10 = Consulta()
+    OP11 = Consulta()
+    OP12 = Consulta()
+    OP13 = Consulta()
+    OP14 = Consulta()
+    OP15 = Consulta()
+
+    @classmethod
+    def get_opcao(cls,num):
+        return cls[f"OP{num}"]
+
+
 
 #descreve as consultas uma a uma.
-listaOpcoesDescricao = [
-"Carros disponíveis por loja e categoria",
-"Frequência que cada carro sofre manutenção ",
-"Frequência que cada modelo decarro sofre manutenção",
-"Carros locados por funcionário",
-"Valores médios de diária" ,
-"Valores médios de tempo de locação",
-"carros em locação  e seus clientes",
-"Clientes atendidos por gerentes ou mecanicos",
-"Custo médio de conserto, consertos mais caros",
-"Acionamento de seguros por mês",
-"gasto médio e frequência de locação, lojas mais usadas por usuário",
-"carro mais alugado de cada loja",
-"Quantas vezes cada forma de pagamento foi utilizado",
-"qual forma de pagamento foi mais utilizada",
-"carros que estão fora da loja",
-    ]
 
 
-#lista de consultas assim como seria enviado ao banco de dados, sem formatação adicional
-listaOpcoesRaw = [
-f'''SELECT IdLoja, IdCarro, AnoFab, ValorDiaria, TipoCombustivel, Placa, Opcionais
+Menu.OP1.value.select = "SELECT IdLoja, IdCarro, AnoFab, ValorDiaria, TipoCombustivel, Placa, Opcionais"
+Menu.OP1.value.descricao = "Carros disponíveis por loja e categoria"
+Menu.OP1.value.comando = f'''SELECT IdLoja, IdCarro, AnoFab, ValorDiaria, TipoCombustivel, Placa, Opcionais
     FROM CARROS1 NATURAL JOIN Lojas
     WHERE  IdCarro NOT IN (SELECT IdCarro FROM Locacoes WHERE DataFinal IS NULL)
     GROUP BY IdLoja,NomeCat,IdCarro,AnoFab, ValorDiaria, TipoCombustivel, Placa, Opcionais
     ORDER BY IdLoja;
-''',
+'''
 
-f'''SELECT Carros1.IdCarro, Placa, COUNT(CARROS1.IdCarro) as QtdManutencoes
+Menu.OP2.value.select = "SELECT Carros1.IdCarro, Placa, COUNT(CARROS1.IdCarro) as QtdManutencoes"
+Menu.OP2.value.descricao = "Frequência que cada carro sofre manutenção"
+Menu.OP2.value.comando = f'''SELECT Carros1.IdCarro, Placa, COUNT(CARROS1.IdCarro) as QtdManutencoes
 from Carros1 join Manutencoes on Carros1.IdCarro = Manutencoes.IdCarro
 group by Carros1.IdCarro,Placa;
-''',
+'''
 
-f'''SELECT NomeCat, COUNT(NomeCat) as "Qtd Manutencoes"
+Menu.OP3.value.select = 'SELECT NomeCat, COUNT(NomeCat) as "Qtd Manutencoes"'
+Menu.OP3.value.descricao = "Frequência que cada modelo decarro sofre manutenção"
+Menu.OP3.value.comando = f'''SELECT NomeCat, COUNT(NomeCat) as "Qtd Manutencoes"
 from Carros1 join Manutencoes on Carros1.IdCarro = Manutencoes.IdCarro
 group by NomeCat;
-''',
+'''
 
-f'''SELECT idFuncionario, Nome, IdLocacao, Carros1.IdCarro,NomeCat,Chassi,AnoFab,ValorDiaria,TipoCombustivel,Placa
+Menu.OP4.value.select = "SELECT idFuncionario, Nome, IdLocacao, Carros1.IdCarro,NomeCat,Chassi,AnoFab,ValorDiaria,TipoCombustivel,Placa"
+Menu.OP4.value.descricao = "Carros locados por funcionário"
+Menu.OP4.value.comando = f'''SELECT idFuncionario, Nome, IdLocacao, Carros1.IdCarro,NomeCat,Chassi,AnoFab,ValorDiaria,TipoCombustivel,Placa
     FROM FUNCIONARIOS NATURAL JOIN LOCACOES JOIN CARROS1 ON CARROS1.IdCarro = Locacoes.idCarro
     ORDER BY IdFuncionario,NomeCat;
-''',
+'''
 
-f'''select round(avg(ValorDiaria),2) as Valor_diaria_medio
+Menu.OP5.value.select = "select round(avg(ValorDiaria),2) as Valor_diaria_medio"
+Menu.OP5.value.descricao = "Valores médios de diária"
+Menu.OP5.value.comando = f'''select round(avg(ValorDiaria),2) as Valor_diaria_medio
 from carros1 join locacoes on carros1.idCarro = locacoes.Idcarro
-''',
+'''
 
-f'''select sum(Duracao)/count(*) as Duracao_Media from
+Menu.OP6.value.select = "select sum(Duracao)/count(*) as Duracao_Media"
+Menu.OP6.value.descricao = "Valores médios de tempo de locação"
+Menu.OP6.value.comando = f'''select sum(Duracao)/count(*) as Duracao_Media from
 (select (coalesce(datafinal,now()) - datainicio) as Duracao
 from carros1 join locacoes on carros1.idCarro = locacoes.Idcarro
 group by datafinal,datainicio) as DuracaoLocacao;
-''',
+'''
 
-f'''SELECT IdLocatario, Nome, Email, Telefone, CARROS1.IdCarro, NomeCat, Placa, ValorDiaria, DataInicio, DataDevPrevista
+Menu.OP7.value.select = "SELECT IdLocatario, Nome, Email, Telefone, CARROS1.IdCarro, NomeCat, Placa, ValorDiaria, DataInicio, DataDevPrevista"
+Menu.OP7.value.descricao = "carros em locação  e seus clientes"
+Menu.OP7.value.comando = f'''SELECT IdLocatario, Nome, Email, Telefone, CARROS1.IdCarro, NomeCat, Placa, ValorDiaria, DataInicio, DataDevPrevista
     FROM LOCATARIOS NATURAL JOIN LOCACOES JOIN CARROS1 ON CARROS1.IdCarro = Locacoes.idCarro
     WHERE DataFinal IS NULL;
-''',
+'''
 
-f'''SELECT DISTINCT Nome, Email, Telefone
+Menu.OP8.value.select = "SELECT DISTINCT Nome, Email, Telefone"
+Menu.OP8.value.descricao = "Clientes atendidos por gerentes ou mecanicos"
+Menu.OP8.value.comando = f'''SELECT DISTINCT Nome, Email, Telefone
         FROM Locatarios
         NATURAL JOIN Locacoes
         LEFT JOIN GERENTES ON Locacoes.IdFuncionario = GERENTES.IdFuncionario
         LEFT JOIN MECANICOS ON Locacoes.IdFuncionario = MECANICOS.IdFuncionario
         WHERE GERENTES.IdFuncionario IS NOT NULL OR MECANICOS.IdFuncionario IS NOT NULL
-''',
+'''
 
-f'''select round(avg(custo),2) as custo_medio, max(custo) as conserto_mais_caro
+Menu.OP9.value.select = "select round(avg(custo),2) as custo_medio, max(custo) as conserto_mais_caro"
+Menu.OP9.value.descricao = "Custo médio de conserto, consertos mais caros"
+Menu.OP9.value.comando = f'''select round(avg(custo),2) as custo_medio, max(custo) as conserto_mais_caro
 from carros1 join consertos on carros1.idcarro = consertos.idcarro
-''',
+'''
 
-f'''select count(mes || '-' || ano) as qtd,mes,ano from (select date_part('month',data) as mes, date_part('year',data) as ano
+Menu.OP10.value.select = "select count(mes || '-' || ano) as qtd,mes,ano from (select date_part('month',data) as mes, date_part('year',data) as ano"
+Menu.OP10.value.descricao = "Acionamento de seguros por mês"
+Menu.OP10.value.comando = f'''select count(mes || '-' || ano) as qtd,mes,ano from (select date_part('month',data) as mes, date_part('year',data) as ano
 from carros1 join acionamentos on carros1.idcarro = acionamentos.idcarro
  group by carros1.idcarro,data) as acionamentos
 group by mes,ano;
-''',
+'''
 
-f'''select round(avg(custoLocacao),2) as Valor_medio_diaria from
+Menu.OP11.value.select = "select round(avg(custoLocacao),2) as Valor_medio_diaria"
+Menu.OP11.value.descricao = "gasto médio e frequência de locação, lojas mais usadas por usuário"
+Menu.OP11.value.comando = f'''select round(avg(custoLocacao),2) as Valor_medio_diaria from
 (select (extract (EPOCH from (coalesce(datafinal,now()) - datainicio))/(3600*24)*ValorDiaria) as CustoLocacao,valorDiaria,datafinal,datainicio
  from carros1 join locacoes on carros1.idCarro = locacoes.Idcarro
 group by datafinal,datainicio,valorDiaria) as CustoLocacao;
-''',
+'''
 
-f'''select placa, idLoja,qtd from
+Menu.OP12.value.select = "select placa, idLoja,qtd"
+Menu.OP12.value.descricao = "carro mais alugado de cada loja"
+Menu.OP12.value.comando = f'''select placa, idLoja,qtd from
 (select  count(*) as qtd,placa,carros1.idLoja
 from locacoes join carros1 on locacoes.idCarro = carros1.idcarro
 group by carros1.idLoja,carros1.idCarro,placa) as subconsulta
 group by placa,IdLoja,qtd
 having qtd = max(qtd)
-''',
-f'''select count(*) filter (where tipopag = false) as qtd_cartao, count(*) filter (where tipopag = true) as qtd_transferencia from carros1 join locacoes on carros1.idcarro = locacoes.idcarro group by tipopag;
-''',
-f'''select greatest(qtd_cartao,qtd_transferencia) as qtd,(qtd_cartao < qtd_transferencia) as tipopag from (select count(*) filter (where tipopag = false) as qtd_cartao, count(*) filter (where tipopag = true) as qtd_transferencia
+'''
+
+Menu.OP13.value.select = "select count(*) filter (where tipopag = false) as qtd_cartao, count(*) filter (where tipopag = true) as qtd_transferencia"
+Menu.OP13.value.descricao = "Quantas vezes cada forma de pagamento foi utilizado"
+Menu.OP13.value.comando = f'''select count(*) filter (where tipopag = false) as qtd_cartao, count(*) filter (where tipopag = true) as qtd_transferencia from carros1 join locacoes on carros1.idcarro = locacoes.idcarro group by tipopag;
+'''
+
+Menu.OP14.value.select = "select greatest(qtd_cartao,qtd_transferencia) as qtd,(qtd_cartao < qtd_transferencia) as tipopag from (select count(*) filter (where tipopag = false) as qtd_cartao, count(*) filter (where tipopag = true) as qtd_transferencia"
+Menu.OP14.value.descricao = "qual forma de pagamento foi mais utilizada"
+Menu.OP14.value.comando = f'''select greatest(qtd_cartao,qtd_transferencia) as qtd,(qtd_cartao < qtd_transferencia) as tipopag from (select count(*) filter (where tipopag = false) as qtd_cartao, count(*) filter (where tipopag = true) as qtd_transferencia
 from carros1 join locacoes on carros1.idcarro = locacoes.idcarro
 group by tipopag) as tipos
  group by qtd_cartao,qtd_transferencia;
-''',
-f'''SELECT IdCarro, PLACA, NomeCat,ValorDiaria
+'''
+
+Menu.OP15.value.select = "SELECT IdCarro, PLACA, NomeCat,ValorDiaria"
+Menu.OP15.value.descricao = "carros que estão fora da loja"
+Menu.OP15.value.comando = f'''SELECT IdCarro, PLACA, NomeCat,ValorDiaria
         FROM CARROS1
         WHERE IdCarro IN (SELECT IdCarros
                           FROM TRANSFERENCIA
@@ -128,7 +157,8 @@ f'''SELECT IdCarro, PLACA, NomeCat,ValorDiaria
                                                                           FROM TRANSFERENCIA
                                                                           WHERE DataSaida > DataFinal))
 '''
-]
+
+
 
 def formatar_consulta(opcao, *args):
     #Itera sobre os valores a serem utilizados nas consultas, que estão no formato variavel = valor, separando os dois itens da string
@@ -143,12 +173,12 @@ def formatar_consulta(opcao, *args):
 
 def listar_consultas():
     print("Menu de consultas\n")
-    for index,opcao in enumerate(listaOpcoesDescricao):
-        print(f"{index}. {opcao}")
+    for item in Menu:
+        print(f"{item.name}. {item.value.descricao}")
 
     opcao = int(input("Escolha a consulta que deseja realizar\n"))
     print(f"\n{opcao=}")
-    print(f"{listaSelect[opcao]} \n\nDentre os valores acima, especifique os que deseja restringir")
+    print(f"{Menu.get_opcao(opcao).value.select} \n\nDentre os valores acima, especifique os que deseja restringir")
 
 def main():
     formatar_consulta(7, "abc=xyz", "a=5", "kj = ast")
